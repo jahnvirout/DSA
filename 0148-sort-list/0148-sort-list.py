240e9f04ea5f5a56513794cd5f47c25938f5837f
Jahnvi rout <168582707+jahnvirout@users.noreply.github.com>
2026-08-03 05:53:49 +0530

    Time: 179 ms (45.63%), Space: 40.8 MB (32.42%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -1,21 +1,48 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
-        temp = head
-        while temp:
-            arr.append(temp.val)
-            temp = temp.next
-        
-        arr.sort()
+
+        # Base case
+        if not head or not head.next:
+            return head
+
+        # Find middle
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Split the list
+        mid = slow.next
+        slow.next = None
+
+        # Sort both halves
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        # Merge two sorted lists
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
 
         dummy = ListNode(0)
-        curr = dummy
-        for num in arr:
-            curr.next = ListNode(num)
-            curr = curr.next
+        temp = dummy
+
+        while l1 and l2:
+
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+
+            temp = temp.next
+
+        if l1:
+            temp.next = l1
+        else:
+            temp.next = l2
+
         return dummy.next
